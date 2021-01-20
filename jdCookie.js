dda8d08703c4fbd5f58a2f9ced573736149e75ee
hujianlong <xznuhjl@sina.com>
2021-01-20 14:17:33 +0800

    脚本修改

--- jdCookie.js
+++ jdCookie.js
@@ -3,7 +3,7 @@
  */
 //此处填写京东账号cookie。
 let CookieJDs = [
-  '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJgBmpoADD9emYuEnUFuFt45cTh6OcDXXeW8XfaXMrrAYd6Gj4DaLXckWH27C_Pw3j8q35Ozwk;pt_pin=15105161554_p;',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
   '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
 // 判断环境变量里面是否有京东ck
